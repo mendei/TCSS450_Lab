107c91e4064f6684057078d63772c9424e39c16b
D. Jared <idledj@uw.edu>
2020-10-21 20:33:12 -0700

    Adding a second comment for jason  - Jared

--- app/src/main/java/edu/uw/tcss450/AuthActivity.java
+++ app/src/main/java/edu/uw/tcss450/AuthActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 //Adding a comment onto your repository -Jared
 public class AuthActivity extends AppCompatActivity {
-
+    //Second comment to show jason
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
